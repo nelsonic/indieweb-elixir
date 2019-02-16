16c0244aa5b4b7e03910d378927b1811ce26ec01
Jacky Alciné <yo@jacky.wtf>
2019-02-15 16:38:29 -0800

    bump(version): Move to 0.0.5.

--- mix.exs
+++ mix.exs
@@ -5,7 +5,7 @@ defmodule IndieWeb.MixProject do
   def project do
     [
       app: :elixir,
-      version: "0.0.4",
+      version: "0.0.5",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
