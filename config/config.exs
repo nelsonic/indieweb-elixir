32f17f1c776a75a5f3008e11e7f5c2d12a8c9e3c
Jacky Alciné <yo@jacky.wtf>
2019-02-26 15:36:56 -0800

    fix(config): Set default adapters.

--- config/config.exs
+++ config/config.exs
@@ -1,3 +1,8 @@
 use Mix.Config
 
+config :indieweb,
+  http_adapter: IndieWeb.Http.Adapters.HTTPotion,
+  cache_adapter: IndieWeb.Cache.Adapters.Cachex,
+  auth_adapter: IndieWeb.Auth.Adapters.Default
+
 import_config "#{Mix.env()}.exs"
